48142454587ff37f83e14342302bc872439e2a82
Scott Hall <scott@distantmusic.com>
2009-08-07 07:51:46 -0400

    Routing fix to allow route to find a freq

--- app/controllers/log_controller.rb
+++ app/controllers/log_controller.rb
@@ -1,18 +1,17 @@
 class LogController < ApplicationController
   def index
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @frequencies }
-    end
+#---     respond_to do |format|
+#---       format.html # index.html.erb
+#---       format.xml  { render :xml => @frequencies }
+#---     end
   end
 
-  # GET /log/find
-  # GET /log/find.xml
   def find
-    @frequencies = Frequency.find(params[0])
+    puts "************* FIND *************\n"
+#    @frequencies = Frequency.find(params[0])
 
     respond_to do |format|
-      format.html # find.html.erb
+      format.html # { render :action => "find" }
       format.xml  { render :xml => @frequency }
     end
   end
