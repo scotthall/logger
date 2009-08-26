343ccfaae6282fbcb072c6f00bb6783f11d4214a
Scott Hall <shall@andromeda.(none)>
2009-08-25 22:39:35 -0400

    Make radio, antenna and modulation mode select lists

--- app/controllers/log_controller.rb
+++ app/controllers/log_controller.rb
@@ -4,11 +4,6 @@ class LogController < ApplicationController
 
   def index
 
-#---     respond_to do |format|
-#---       format.html # index.html.erb
-#---       format.xml  { render :xml => @frequencies }
-#---     end
-
   end  #--- index
 
   def find
@@ -17,55 +12,13 @@ class LogController < ApplicationController
 
       @search_freq = ""
 
-      printf("************* FIND POST params[:search_freq] [ %s ] *************\n", params[:search_freq])
-
       search_freq = params[:search_freq]
-      printf("************* search_freq [ %s ] *************\n", search_freq)
 
-#---      @frequencies =
-#---        Frequency.find(:all, :conditions => ["frequency = ?" , search_freq])
-#---
-#---      if (@frequencies.empty?)
-#---
-#---        printf("------------- frequencies is EMPTY -------------\n")
-#---        render :template=> "log/not_found"
-#---
-#---      else
-#---
-#---        render :template=> "log/show"
-#---
-#---        for frequency in @frequencies
-#---
-#---          printf("frequency = %f\n", frequency.frequency)
-#---          printf("modulation = %s\n", frequency.modulation)
-#---          printf("radio = %s\n", frequency.radio)
-#---          printf("antenna = %s\n", frequency.antenna)
-#---          printf("date = %s\n", frequency.date)
-#---
-#---          printf("call = %s\n", frequency.call)
-#---          printf("digimode = %s\n", frequency.digimode)
-#---          printf("own_station_rst = %s\n", frequency.own_station_rst)
-#---          printf("peer_station_rst = %s\n", frequency.peer_station_rst)
-#---          printf("s_meter = %s\n", frequency.s_meter)
-#---          printf("name = %s\n", frequency.name)
-#---          printf("qth = %s\n", frequency.qth)
-#---          printf("locator = %s\n", frequency.locator)
-#---          printf("comment = %s\n", frequency.comment)
-#---
-#---        end  #--- for frequency in @frequencies
-#---
-#---      end  #--- if (@frequencies.empty?)
-
-#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
-#---      @frequencies =
-#---        Frequency.find(:all, :conditions => "frequency = search_freq")
       @frequencies =
-        Frequency.find(:all, :conditions => "frequency = 12.34")
+        Frequency.find(:all, :conditions => ["frequency = ?" , search_freq])
 
       if (@frequencies.empty?)
 
-        printf("------------- frequencies is EMPTY -------------\n")
         render :template=> "log/not_found"
 
       else
@@ -93,8 +46,6 @@ class LogController < ApplicationController
         end  #--- for frequency in @frequencies
 
       end  #--- if (@frequencies.empty?)
-
-#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
     else
 
--- app/models/frequency.rb
+++ app/models/frequency.rb
@@ -1,2 +1,55 @@
 class Frequency < ActiveRecord::Base
-end
+
+#--- Radio: a = IC-756PROIII, b = IC-R71, c = HP3586B, d = CRYSTAL, e = IC-PRC2500
+#--- Antenna B = Butternut, L = long wire
+
+  RADIOS = [
+
+    # ---------------------------------------------------------------------- #
+    #  DISPLAYED       STORED IN DB                                          #
+    # ---------------------------------------------------------------------- #
+    [ "IC-756PROIII", "IC-756PROIII" ],
+    [ "IC-R71",       "IC-R71"       ],
+    [ "HP3586B",      "HP3586B"      ],
+    [ "CRYSTAL",      "CRYSTAL"      ],
+    [ "IC-PRC2500",   "IC-PRC2500"   ]
+
+  ]  #--- RADIOS
+
+  ANTENNAS = [
+
+    # ---------------------------------------------------------------------- #
+    #  DISPLAYED       STORED IN DB                                          #
+    # ---------------------------------------------------------------------- #
+    [ "Butternut", "Butternut" ],
+    [ "long wire", "long wire" ],
+    [ "10 meter",  "10 meter"  ]
+
+  ]  #--- ANTENNAS
+
+  MODULATION_MODES = [
+
+    # ---------------------------------------------------------------------- #
+    #  DISPLAYED       STORED IN DB                                          #
+    # ---------------------------------------------------------------------- #
+    [ "USB",  "USB"  ],
+    [ "LSB",  "LSB"  ],
+    [ "AM",   "AM"   ],
+    [ "RTTY", "RTTY" ],
+    [ "CW",   "CW"   ],
+    [ "FMW",  "FMW"  ],
+    [ "FMN",  "FMN"  ]
+
+  ]  #--- MODULATION_MODES
+
+  validates_presence_of :frequency, :modulation, :radio, :date, :comment, 
+                        :antenna, :s_meter
+
+#---   validates_inclusion_of :radio, :in => 
+#---     RADIOS.map {|disp, value| value}
+#---   validates_inclusion_of :antenna, :in => 
+#---     ANTENNAS.map {|disp, value| value}
+#---   validates_inclusion_of :modulation, :in => 
+#---     MODULATION_MODES.map {|disp, value| value}
+
+end  #--- class Frequency
